444e9d134d6a82c3294337f6d0b5d788a52eec0e
Saad Ahmed <dropheaven@live.com>
2019-06-29 21:32:36 -0400

    use bootstrap's card container

--- client/src/components/ExpenseItem.js
+++ client/src/components/ExpenseItem.js
@@ -1,12 +1,23 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ExpenseItem = ({ title, amount, dueDate }) => {
   return (
-    <Fragment>
-      <p>title: {title}</p>
-      <p>amount: {amount}</p>
-      <p>due date: {dueDate}</p>
-    </Fragment>
+    <div className="card border-dark m-2 text-center" id="expense-card">
+      <div className="card-header bg-transparent border-dark">{title}</div>
+      <div className="card-body">
+        <h4 className="card-title">${amount}</h4>
+        <p className="card-text">Due on {dueDate}</p>
+      </div>
+      <div className="card-footer bg-transparent border-dark">
+        <Link to={`/expenses/${title}`}>
+          <button type="button" className="btn btn-primary">details</button>
+        </Link>
+        <Link to={`/expenses/${title}/edit`}>
+          <button type="button" className="btn btn-primary">edit</button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
