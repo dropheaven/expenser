561b53e85362b9b1278d674a7ec58baf461382a6
Saad Ahmed <dropheaven@live.com>
2019-06-29 20:48:37 -0400

    add NavLinks for dashboard and add expense

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => (
-  <nav id="sidebar" className="min-vh-100">
-    navbar
+  <nav id="sidebar" className="min-vh-100 text-center">
+    <h2 className="pt-2">Expenser</h2>
+    <NavLink exact to="/" className="d-block">dashboard</NavLink>
+    <NavLink to="/expense/new" className="d-block">add expense</NavLink>
   </nav>
 );
 
