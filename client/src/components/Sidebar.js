7a1d9c95719b8fb4a40b795a4e1cbee1fa089710
Saad Ahmed <dropheaven@live.com>
2019-06-30 20:00:29 -0400

    new link for login page

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -6,6 +6,7 @@ const Sidebar = () => (
     <h2 className="pt-2">Expenser</h2>
     <NavLink exact to="/" className="d-block">dashboard</NavLink>
     <NavLink to="/expense/new" className="d-block">add expense</NavLink>
+    <NavLink to="/login" className="d-block">login</NavLink>
   </nav>
 );
 
